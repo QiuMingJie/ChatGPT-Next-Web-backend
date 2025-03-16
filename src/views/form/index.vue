3d38a65ca859b9fdf8ce52f1992dd791e59a5bda
cain <826292460@qq.com>
2025-03-17 00:01:45 +0800

    提交

--- src/views/form/index.vue
+++ src/views/form/index.vue
@@ -2,18 +2,27 @@
   <div class="dashboard-container">
     <div class="dashboard-search">
       <el-form :inline="true" :model="form">
-        <el-form-item label="deepseek回答">
-          <el-input size="small" v-model="form.answer" placeholder="输入deepseek回答" clearable @keyup.enter.native="initData"/>
+        <el-form-item label="用户ID">
+          <el-input size="small" v-model="form.userId" placeholder="输入用户ID" clearable @keyup.enter.native="initData"/>
         </el-form-item>
-        <el-form-item label="问题的原文">
-          <el-input size="small" v-model="form.orgQuestion" placeholder="输入问题的原文" clearable @keyup.enter.native="initData"/>
+        <el-form-item label="用户名">
+          <el-input size="small" v-model="form.userName" placeholder="输入用户名" clearable @keyup.enter.native="initData"/>
         </el-form-item>
-        <el-form-item label="发给deepseek的问题">
+        <el-form-item label="问题">
           <el-input size="small" v-model="form.toDeepSeekQuestion" placeholder="输入发给deepseek的问题" clearable @keyup.enter.native="initData"/>
         </el-form-item>
-        <el-form-item label="userId">
-          <el-input size="small" v-model="form.userId" placeholder="输入发给deepseek的问题" clearable @keyup.enter.native="initData"/>
+        <el-form-item label="回答">
+          <el-input size="small" v-model="form.answer" placeholder="输入deepseek回答" clearable @keyup.enter.native="initData"/>
         </el-form-item>
+        <el-form-item label="聊天类型">
+          <el-select v-model="form.chatType" >
+            <el-option label="中医诊断学知识点查询库" value="中医诊断学知识点查询库"></el-option>
+            <el-option label="中医诊断学AI出题" value="中医诊断学AI出题"></el-option>
+            <el-option label="中医诊断学智能答疑" value="中医诊断学智能答疑"></el-option>
+          </el-select>
+        </el-form-item>
+       
+        
         <el-form-item>
           <el-button size="small" type="primary" @click="initData">查询</el-button>
           <el-button size="small" @click="resetFn">重置</el-button>
@@ -21,32 +30,12 @@
       </el-form>
     </div>
     <div class="dashboard-table">
-      <el-table :data="tableData" style="width: 100%" v-loading="loading">
+      <el-table :data="tableData" border style="width: 100%" v-loading="loading">
         <el-table-column type="index" label="序号" width="80"  />
-        <el-table-column prop="id" label="问题id" width="180" />
-        <el-table-column prop="userId" label="问问题的人的id" width="120" />
-        <el-table-column prop="answer" label="deepseek回答" width="360">
-          <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <div>{{ scope.row.answer }}</div>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.answer }}</el-tag>
-              </div>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column prop="toDeepSeekQuestion" label="发给deepseek的问题" width="360">
-          <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <div>{{ scope.row.toDeepSeekQuestion }}</div>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">{{ scope.row.toDeepSeekQuestion }}</el-tag>
-              </div>
-            </el-popover>
-          </template>
-        </el-table-column>
-        <el-table-column prop="datetime" label="问的时间" width="200" />
-        <el-table-column prop="orgQuestion" label="问题的原文" width="200">
+        <el-table-column prop="id" label="问题ID" width="180" resizable/>
+        <el-table-column prop="userId" label="用户ID" width="120" resizable />
+        <el-table-column prop="userName" label="用户名" width="120" resizable />
+        <el-table-column prop="orgQuestion" label="问题" width="200" resizable>
           <template slot-scope="scope">
             <el-popover trigger="hover" placement="top">
               <div>{{ scope.row.orgQuestion }}</div>
@@ -56,7 +45,17 @@
             </el-popover>
           </template>
         </el-table-column>
-        <el-table-column prop="remark" label="备注" width="220" />
+        <el-table-column prop="answer" label="AI 回答" minWidth="360" resizable>
+          <template slot-scope="scope">
+            <el-popover trigger="hover" placement="top">
+              <div>{{ scope.row.answer }}</div>
+              <div slot="reference" class="name-wrapper">
+                <el-tag size="medium">{{ scope.row.answer }}</el-tag>
+              </div>
+            </el-popover>
+          </template>
+        </el-table-column>
+        <el-table-column prop="datetime" label="时间" width="200"  resizable/>
       </el-table>
       <div class="disboard-page">
         <el-pagination
@@ -87,12 +86,13 @@ export default {
       loading: false,
       tableData: [],
       form: {
+        userId: "",
+        userName: "",
+        chatType: "",
         answer: "",
         orgQuestion: "",
-        userId: "",
         sortBy: "datetime",
         sortDirection: "desc", 
-        toDeepSeekQuestion: ""
       },
       pageParam: {
         page: 1,
@@ -107,9 +107,9 @@ export default {
       try{
         this.loading = true
         await searchTCMchat({ ...param }).then((res) => {
-          const {content, totalPages} = res.data
+          const {content, totalElements} = res.data
           this.tableData = content
-          this.pageParam.total = totalPages
+          this.pageParam.total = totalElements
           this.loading = false
         })
       }catch(error){
